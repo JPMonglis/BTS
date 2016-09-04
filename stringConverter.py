719e2a37c65c597c9c7348b3680de7e6c0b217f4
palaine <paullaine@protonmail.com>
2016-09-04 14:41:21 +0200

    Add the URL encode/decode on the stringconverter.py script

--- stringConverter.py
+++ stringConverter.py
@@ -5,13 +5,14 @@ import os
 import re
 import base64
 import binascii
-
+import urllib
 
 # \
 # Author: Paul Laîné
 # Data: Sat Sep, 2016
 # Version: 1.0
 # /
+
 
 def file_base64_to_text():
     try:
@@ -170,6 +171,57 @@ def hexadecimal_to_text():
                     print "Your hexadecimal input don't work..."
             else:
                 print "Input a hexadecimal please"
+            if raw_input("\nAnother time (Y/N): ") in ('N', 'n'):
+                break
+    except KeyboardInterrupt:
+        print "\nHave a nice day 1337."
+        sys.exit()
+
+
+def file_url_to_text():
+    try:
+        path = raw_input("\nPlease enter the path of you file: ")
+        try:
+            data = open(path, 'r')
+            content = data.read()
+            print "[+] Start encoding the URL..."
+            try:
+                plain = urllib.unquote(content)
+            except TypeError:
+                return "Your URL content don't work..."
+            print "[+] Start encoding the URL ... OK"
+            data.close()
+        except IOError:
+            return "File not found ... "
+        save = raw_input("Do you want to save the plain text (Y/N): ")
+        if save == "y" or save == "Y":
+            path_save = raw_input("Where you want to save the cypher text: ")
+            try:
+                print "[+] Trying to generate the file ..."
+                data = open(path_save, 'a')
+                print "[+] Trying to generate the file ... OK"
+                print "[+] Start writing the cypher text on the file ..."
+                data.write(plain)
+                print "[+] Start writing the cypher text on the file ... OK"
+                data.close()
+            except IOError:
+                return "Ooops something goes wrong with your file, retry ..."
+        raw_input("\nHit any key to continue ...")
+        return plain
+    except KeyboardInterrupt:
+        print "\nHave a nice day 1337."
+        sys.exit()
+
+
+def url_to_text():
+    try:
+        while True:
+            print "\nInput URL: "
+            cypher = raw_input("Cypher> ")
+            try:
+                print "Plain> " + urllib.unquote(cypher)
+            except TypeError:
+                print "Your URL don't work..."
             if raw_input("\nAnother time (Y/N): ") in ('N', 'n'):
                 break
     except KeyboardInterrupt:
@@ -341,6 +393,60 @@ def text_to_hexadecimal():
         sys.exit()
 
 
+def file_text_to_url():
+    try:
+        path = raw_input("\nPlease enter the path of you file: ")
+        try:
+            data = open(path, 'r')
+            content = data.read()
+            print "[+] Start decoding the URL..."
+            try:
+                plain = urllib.quote_plus(content)
+            except TypeError:
+                return "Your URL content don't work..."
+            print "[+] Start decoding the URL ... OK"
+            data.close()
+        except IOError:
+            return "File not found ... "
+        save = raw_input("Do you want to save the plain text (Y/N): ")
+        if save == "y" or save == "Y":
+            path_save = raw_input("Where you want to save the plain text: ")
+            try:
+                print "[+] Trying to generate the file ..."
+                data = open(path_save, 'a')
+                print "[+] Trying to generate the file ... OK"
+                print "[+] Start writing the plain text on the file ..."
+                data.write(plain)
+                print "[+] Start writing the plain text on the file ... OK"
+                data.close()
+            except IOError:
+                return "Ooops something goes wrong with your file, retry ..."
+        raw_input("\nHit any key to continue ...")
+        return plain
+    except KeyboardInterrupt:
+        print "\nHave a nice day 1337."
+        sys.exit()
+
+
+def text_to_url():
+    try:
+        while True:
+            print "\nInput URL: "
+            cypher = raw_input("Cypher> ")
+            if re.match("^(https?://|www\.)[a-z-]+\.[a-z0-9-]+[a-z.]{0,4}/?[a-z0-9A-Z*_\-'();:@&=+$,/?#.]+$", cypher):
+                try:
+                    print "Plain> "+urllib.quote_plus(cypher)
+                except TypeError:
+                    print "Your URL don't work..."
+            else:
+                print "Input a good URL please"
+            if raw_input("\nAnother time (Y/N): ") in ('N', 'n'):
+                break
+    except KeyboardInterrupt:
+        print "\nHave a nice day 1337."
+        sys.exit()
+
+
 def report(info):
     return "+" + "-" * 20 + "   REPORT   " + "-" * 20+"\n"+info+"\n+" + "-" * 52 + "\n"
 
@@ -358,9 +464,10 @@ def menu(info_version):
             os.system('clear')
             print notifications
             print "\t\t  " + style_on + " M A I N - M E N U " + style_off
-            print "\n    01. Base 64 to text\t\t 04. Text to base 64"
-            print "    02. Binary  to text\t\t 05. Text to binary"
-            print "    03. Hexa    to text\t\t 06. Text to hexadecimal"
+            print "\n    01. Base 64 to text\t\t 05. Text to base 64"
+            print "    02. Binary  to text\t\t 06. Text to binary"
+            print "    03. Hexa    to text\t\t 07. Text to hexadecimal"
+            print "    04. URL     to text\t\t 08. Text to URL"
             print "\n\t\t\t66. EXIT\n"
             rep = raw_input('strC0nv3rt/menu> ')
             if rep == 1 or rep == "1" and set_version == 2:
@@ -382,22 +489,34 @@ def menu(info_version):
                 hexadecimal_to_text()
 
             elif rep == 4 or rep == "4" and set_version == 2:
-                cypher = file_text_to_base64()
-                notifications = report(cypher)
+                plain = file_url_to_text()
+                notifications = report(plain)
             elif rep == 4 or rep == "4" and set_version == 1:
-                text_to_base64()
+                url_to_text()
 
             elif rep == 5 or rep == "5" and set_version == 2:
-                cypher = file_text_to_binary()
+                cypher = file_text_to_base64()
                 notifications = report(cypher)
             elif rep == 5 or rep == "5" and set_version == 1:
-                text_to_binary()
+                text_to_base64()
 
             elif rep == 6 or rep == "6" and set_version == 2:
-                cypher = file_text_to_hexadecimal()
+                cypher = file_text_to_binary()
                 notifications = report(cypher)
             elif rep == 6 or rep == "6" and set_version == 1:
+                text_to_binary()
+
+            elif rep == 7 or rep == "7" and set_version == 2:
+                cypher = file_text_to_hexadecimal()
+                notifications = report(cypher)
+            elif rep == 7 or rep == "7" and set_version == 1:
                 text_to_hexadecimal()
+
+            elif rep == 8 or rep == "8" and set_version == 2:
+                cypher = file_text_to_url()
+                notifications = report(cypher)
+            elif rep == 8 or rep == "8" and set_version == 1:
+                text_to_url()
 
             elif rep == 66 or rep == "66":
                 os.system('clear')
